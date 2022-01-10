5100ada80f50e478ed03f6639983d2e70a570e1b
Mauricio <mngoliveira@gmail.com>
2022-01-10 19:19:15 -0300

    Primeiro Commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final List _todoList = ['Mauricio', 'Marcos', 'Daniel', 'Sabrina', 'João'];
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  late Map<String, dynamic> _lastRemoved;
+  late int _lastRemovedPos;
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data!);
+      });
+    });
+  }
+
+  void _addToDo() {
+    setState(() {
+      // {} literal conversão do Map()
+      Map<String, dynamic> newTodo = {};
+      newTodo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newTodo["ok"] = false;
+      _toDoList.add(newTodo);
+      _saveData();
+    });
+  }
+
+  Future<void> _refresh() async {
+    await Future.delayed(
+      const Duration(seconds: 1),
+    );
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"]) {
+          return 1;
+        } else if (b["ok"] && !a["ok"]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+      _saveData();
+    });
+    // ignore: avoid_returning_null_for_void
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +82,10 @@ class _HomeState extends State<Home> {
             padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
             child: Row(
               children: [
-                const Expanded(
+                Expanded(
                   child: TextField(
-                    decoration: InputDecoration(
+                    controller: _toDoController,
+                    decoration: const InputDecoration(
                       labelText: "Nova Tarefa",
                       labelStyle: TextStyle(color: Colors.blueAccent),
                     ),
@@ -48,25 +96,85 @@ class _HomeState extends State<Home> {
                     primary: Colors.blueAccent, // background
                     onPrimary: Colors.white, // foreground
                   ),
-                  onPressed: () {},
+                  onPressed: _addToDo,
                   child: const Text('ADD'),
                 )
               ],
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.only(top: 10.0),
-              itemCount: _todoList.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_todoList[index]),
-                );
-              },
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
             ),
-          )
+          ),
         ],
       ),
+    );
+  }
+
+  Widget buildItem(BuildContext context, int index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      direction: DismissDirection.startToEnd,
+      background: Container(
+          color: Colors.red,
+          child: const Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(Icons.delete),
+          )),
+      child: CheckboxListTile(
+        onChanged: (click) {
+          setState(() {
+            _toDoList[index]["ok"] = click;
+            _saveData();
+          });
+        },
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(
+            _toDoList[index]["ok"] ? Icons.check : Icons.error,
+          ),
+        ),
+      ),
+      onDismissed: (direction) {
+        setState(
+          () {
+            _lastRemoved = Map.from(_toDoList[index]);
+            _lastRemovedPos = index;
+            _toDoList.removeAt(index);
+
+            _saveData();
+
+            final snack = SnackBar(
+              action: SnackBarAction(
+                label: 'Desfazer',
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                },
+              ),
+              content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+              duration: const Duration(seconds: 3),
+              // width: 280.0, // Width of the SnackBar.
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0, // Inner padding for SnackBar content.
+              ),
+              behavior: SnackBarBehavior.floating,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snack);
+          },
+        );
+      },
     );
   }
 
@@ -78,7 +186,7 @@ class _HomeState extends State<Home> {
 
 // Função para salvar os dados(Todo List em Json)
   Future<File> _saveData() async {
-    String data = json.encode(_todoList);
+    String data = json.encode(_toDoList);
     final file = await _getFile();
     return file.writeAsString(data);
   }
